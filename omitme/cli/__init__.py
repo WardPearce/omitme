b50f57a8825e35190f871b03eff039189fe81063
WardPearce <wardpearce@protonmail.com>
2023-08-21 08:55:50 +1200

    Handle passing account context

--- omitme/cli/__init__.py
+++ omitme/cli/__init__.py
@@ -18,6 +18,37 @@ for platform in PLATFORMS:
     def platform_group() -> None:
         pass
 
-    @platform_group.command("login")
-    def login(platform=platform) -> None:
+    @platform_group.command("login", help="Log into a new account")
+    @click.option(
+        "--nosave",
+        default=False,
+        type=click.BOOL,
+        help=f"Saves session details for {platform.alias} in a temp file",
+    )
+    def login(nosave: bool, platform=platform) -> None:
         asyncio.run(platform().handle_login())
+
+    @platform_group.command("accounts", help="List accounts already logged in")
+    def accounts() -> None:
+        pass
+
+    @platform_group.group(
+        "targets", help="Run specific targets on the selected platform"
+    )
+    @click.option(
+        "--account",
+        prompt="Your account name",
+        help="Provide the name of the account you want to commit a action on",
+        required=True,
+    )
+    @click.pass_context
+    def targets(ctx, account: str) -> None:
+        ctx.obj = {}
+        ctx.obj["account"] = account
+
+    for method, meta in platform._target_methods:
+
+        @targets.command(meta.action.replace(" ", "-"), help=meta.description)
+        @click.pass_context
+        def handle_target(ctx) -> None:
+            pass
