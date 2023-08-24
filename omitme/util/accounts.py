fe12a2bc5d2cfadbdfa85b4540510a8c281ce008
WardPearce <wardpearce@protonmail.com>
2023-08-24 14:01:01 +1200

    Added keyring & encrypted session support

--- omitme/util/accounts.py
+++ omitme/util/accounts.py
@@ -5,6 +5,8 @@ from urllib.parse import quote_plus, unquote_plus
 
 import aiofiles
 import aiofiles.os
+import keyring
+from cryptography.fernet import Fernet
 
 SYSTEM = platform.system()
 if SYSTEM == "Linux":
@@ -16,6 +18,13 @@ elif SYSTEM == "Darwin":
 else:
     raise Exception("Platform not supported.")
 
+
+__key_raw = keyring.get_password("omitme", "fernet")
+if __key_raw is None:
+    RAW_FERNET_KEY = Fernet.generate_key()
+    keyring.set_password("omitme", "fernet", RAW_FERNET_KEY.hex())
+else:
+    RAW_FERNET_KEY = bytes.fromhex(__key_raw)
 
 if not path.exists(PATHWAY):
     mkdir(PATHWAY)
@@ -30,7 +39,7 @@ class Accounts:
         return quote_plus(self.__platform)
 
     def __json_store_path(self, username: str) -> str:
-        return path.join(PATHWAY, f"{self.__safe_platform}-{quote_plus(username)}.json")
+        return path.join(PATHWAY, f"{self.__safe_platform}-{quote_plus(username)}.bin")
 
     async def remove(self, username: str) -> None:
         await aiofiles.os.remove(self.__json_store_path(username))
@@ -38,17 +47,17 @@ class Accounts:
     async def add(self, username: str, session: dict) -> None:
         async with aiofiles.open(
             self.__json_store_path(username),
-            "w+",
+            "wb+",
         ) as f_:
-            await f_.write(json.dumps(session))
+            await f_.write(Fernet(RAW_FERNET_KEY).encrypt(json.dumps(session).encode()))
 
     async def get(self, username: str) -> dict:
-        async with aiofiles.open(self.__json_store_path(username), "r") as f_:
-            return json.loads(await f_.read())
+        async with aiofiles.open(self.__json_store_path(username), "rb") as f_:
+            return json.loads(Fernet(RAW_FERNET_KEY).decrypt(await f_.read()))
 
     async def list(self) -> list[str]:
         return [
-            unquote_plus(file.removeprefix(f"{self.__platform}-").removesuffix(".json"))
+            unquote_plus(file.removeprefix(f"{self.__platform}-").removesuffix(".bin"))
             for file in await aiofiles.os.listdir(PATHWAY)
             if file.startswith(self.__platform)
         ]
