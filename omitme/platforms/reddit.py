e73e0087ef2072e4ca6111d4e855aadc080ec173
WardPearce <wardpearce@protonmail.com>
2023-08-24 14:53:36 +1200

    Started reddit

--- omitme/platforms/reddit.py
+++ omitme/platforms/reddit.py
@@ -1,3 +1,5 @@
+from typing import AsyncIterator
+
 import httpx
 from selenium.common.exceptions import TimeoutException
 from seleniumwire import webdriver
@@ -5,8 +7,9 @@ from seleniumwire.request import HTTPHeaders
 
 from omitme.errors import LoginError
 from omitme.util.accounts import Accounts
+from omitme.util.events import CheckingEvent, FailEvent, OmittedEvent
 from omitme.util.platform import Platform
-from omitme.util.targets import login
+from omitme.util.targets import login, target
 from omitme.util.wait_for import wait_until_or_close
 
 
@@ -39,12 +42,37 @@ class Reddit(Platform):
         headers.pop("content-length")
         headers.pop("content-type")
 
-        session = httpx.AsyncClient(headers=headers)
+        to_save = {
+            "headers": headers,
+            "params": {"redditWebClient": "web2x", "app": "web2x-client-production"},
+        }
 
-        resp = await session.get(
-            "https://gateway.reddit.com/desktopapi/v1/prefs?redditWebClient=web2x&app=web2x-client-production&include=identity"
-        )
+        session = httpx.AsyncClient(**to_save)  # type: ignore
 
-        await accounts.add(resp.json()["account"]["email"], {"headers": headers})
+        await accounts.add(await self.get_username(session), to_save)
 
         return session
+
+    async def _delete_post(self, session: httpx.AsyncClient, post_id: str) -> None:
+        resp = await session.post(
+            "api/del",
+            params={"raw_json": "1", "gilding_detail": "1"},
+            json={"id": post_id},
+        )
+
+    async def get_username(self, session: httpx.AsyncClient) -> str:
+        resp = await session.get(
+            "https://gateway.reddit.com/desktopapi/v1/prefs",
+            params={"include": "identity"},
+        )
+
+        return resp.json()["account"]["displayText"]
+
+    @target("posts delete", description="Delete all reddit posts")
+    async def handle_delete_posts(
+        self, session: httpx.AsyncClient
+    ) -> AsyncIterator[OmittedEvent | CheckingEvent | FailEvent]:
+        resp = await session.get(f"/user/{await self.get_username(session)}/submitted")
+        print(resp.json()["data"]["children"][0]["data"]["id"])
+
+        yield CheckingEvent(channel="")
